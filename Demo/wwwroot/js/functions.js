f534b2644dbc69aa7fb127cb36bb7fe3637b186e
Juan Esteban <ycjuanesteban@gmail.com>
2020-02-14 06:38:18 +0100

    Interoperabilidad con JS

---
+++ Demo/wwwroot/js/functions.js
@@ -0,0 +1,24 @@
+﻿function showAlert() {
+    alert("Ping!");
+}
+
+function showQuestion(question) {
+    return prompt(question);
+}
+
+function setElementById(id, text) {
+    document.getElementById(id).innerText = text;
+}
+
+function setFocus(element)
+{
+    element.focus();
+}
+
+function callDotNetMethod() {
+    alert('In');
+    DotNet.invokeMethodAsync('ServerBlazor', 'GenerateRandomInt')
+        .then((result) => {
+            setElementById('random-span', result);
+        })
+}
